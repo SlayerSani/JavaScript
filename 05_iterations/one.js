bc58e5d919a595ddb5b1ad5467a2a04197645a03
SlayerSani <sanisourin1999@gmail.com>
2024-02-27 00:43:02 +0530

    one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,360 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if(element == 5){
+        // console.log(`5 is best number`);
+    }
+    // console.log(element);
+}
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5 is best number
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// console.log(element);  //element is not defined
+
+
+for (let i = 0; i <= 10; i++) {
+    // const element = array[i];
+    // console.log(`Outer loop value ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // const element = array[j];
+        // console.log(`Inner loop value ${j} & inner lop ${i}`);
+        // console.log(i *  + j + " = " + i*j );
+    }
+    
+}
+
+// Outer loop value 0
+// Inner loop value 0 & inner lop 0
+// Inner loop value 1 & inner lop 0
+// Inner loop value 2 & inner lop 0
+// Inner loop value 3 & inner lop 0
+// Inner loop value 4 & inner lop 0
+// Inner loop value 5 & inner lop 0
+// Inner loop value 6 & inner lop 0
+// Inner loop value 7 & inner lop 0
+// Inner loop value 8 & inner lop 0
+// Inner loop value 9 & inner lop 0
+// Inner loop value 10 & inner lop 0
+// Outer loop value 1
+// Inner loop value 0 & inner lop 1
+// Inner loop value 1 & inner lop 1
+// Inner loop value 2 & inner lop 1
+// Inner loop value 3 & inner lop 1
+// Inner loop value 4 & inner lop 1
+// Inner loop value 5 & inner lop 1
+// Inner loop value 6 & inner lop 1
+// Inner loop value 7 & inner lop 1
+// Inner loop value 8 & inner lop 1
+// Inner loop value 9 & inner lop 1
+// Inner loop value 10 & inner lop 1
+// Outer loop value 2
+// Inner loop value 0 & inner lop 2
+// Inner loop value 1 & inner lop 2
+// Inner loop value 2 & inner lop 2
+// Inner loop value 3 & inner lop 2
+// Inner loop value 4 & inner lop 2
+// Inner loop value 5 & inner lop 2
+// Inner loop value 6 & inner lop 2
+// Inner loop value 7 & inner lop 2
+// Inner loop value 8 & inner lop 2
+// Inner loop value 9 & inner lop 2
+// Inner loop value 10 & inner lop 2
+// Outer loop value 3
+// Inner loop value 0 & inner lop 3
+// Inner loop value 1 & inner lop 3
+// Inner loop value 2 & inner lop 3
+// Inner loop value 3 & inner lop 3
+// Inner loop value 4 & inner lop 3
+// Inner loop value 5 & inner lop 3
+// Inner loop value 6 & inner lop 3
+// Inner loop value 7 & inner lop 3
+// Inner loop value 8 & inner lop 3
+// Inner loop value 9 & inner lop 3
+// Inner loop value 10 & inner lop 3
+// Outer loop value 4
+// Inner loop value 0 & inner lop 4
+// Inner loop value 1 & inner lop 4
+// Inner loop value 2 & inner lop 4
+// Inner loop value 3 & inner lop 4
+// Inner loop value 4 & inner lop 4
+// Inner loop value 5 & inner lop 4
+// Inner loop value 6 & inner lop 4
+// Inner loop value 7 & inner lop 4
+// Inner loop value 8 & inner lop 4
+// Inner loop value 9 & inner lop 4
+// Inner loop value 10 & inner lop 4
+// Outer loop value 5
+// Inner loop value 0 & inner lop 5
+// Inner loop value 1 & inner lop 5
+// Inner loop value 2 & inner lop 5
+// Inner loop value 3 & inner lop 5
+// Inner loop value 4 & inner lop 5
+// Inner loop value 5 & inner lop 5
+// Inner loop value 6 & inner lop 5
+// Inner loop value 7 & inner lop 5
+// Inner loop value 8 & inner lop 5
+// Inner loop value 9 & inner lop 5
+// Inner loop value 10 & inner lop 5
+// Outer loop value 6
+// Inner loop value 0 & inner lop 6
+// Inner loop value 1 & inner lop 6
+// Inner loop value 2 & inner lop 6
+// Inner loop value 3 & inner lop 6
+// Inner loop value 4 & inner lop 6
+// Inner loop value 5 & inner lop 6
+// Inner loop value 6 & inner lop 6
+// Inner loop value 7 & inner lop 6
+// Inner loop value 8 & inner lop 6
+// Inner loop value 9 & inner lop 6
+// Inner loop value 10 & inner lop 6
+// Outer loop value 7
+// Inner loop value 0 & inner lop 7
+// Inner loop value 1 & inner lop 7
+// Inner loop value 2 & inner lop 7
+// Inner loop value 3 & inner lop 7
+// Inner loop value 4 & inner lop 7
+// Inner loop value 5 & inner lop 7
+// Inner loop value 6 & inner lop 7
+// Inner loop value 7 & inner lop 7
+// Inner loop value 8 & inner lop 7
+// Inner loop value 9 & inner lop 7
+// Inner loop value 10 & inner lop 7
+// Outer loop value 8
+// Inner loop value 0 & inner lop 8
+// Inner loop value 1 & inner lop 8
+// Inner loop value 2 & inner lop 8
+// Inner loop value 3 & inner lop 8
+// Inner loop value 4 & inner lop 8
+// Inner loop value 5 & inner lop 8
+// Inner loop value 6 & inner lop 8
+// Inner loop value 7 & inner lop 8
+// Inner loop value 8 & inner lop 8
+// Inner loop value 9 & inner lop 8
+// Inner loop value 10 & inner lop 8
+// Outer loop value 9
+// Inner loop value 0 & inner lop 9
+// Inner loop value 1 & inner lop 9
+// Inner loop value 2 & inner lop 9
+// Inner loop value 3 & inner lop 9
+// Inner loop value 4 & inner lop 9
+// Inner loop value 5 & inner lop 9
+// Inner loop value 6 & inner lop 9
+// Inner loop value 7 & inner lop 9
+// Inner loop value 8 & inner lop 9
+// Inner loop value 9 & inner lop 9
+// Inner loop value 10 & inner lop 9
+// Outer loop value 10
+// Inner loop value 0 & inner lop 10
+// Inner loop value 1 & inner lop 10
+// Inner loop value 2 & inner lop 10
+// Inner loop value 3 & inner lop 10
+// Inner loop value 4 & inner lop 10
+// Inner loop value 5 & inner lop 10
+// Inner loop value 6 & inner lop 10
+// Inner loop value 7 & inner lop 10
+// Inner loop value 8 & inner lop 10
+// Inner loop value 9 & inner lop 10
+// Inner loop value 10 & inner lop 10
+
+
+
+// Outer loop value 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// 0 = 0
+// Outer loop value 1
+// 0 = 0
+// 1 = 1
+// 2 = 2
+// 3 = 3
+// 4 = 4
+// 5 = 5
+// 6 = 6
+// 7 = 7
+// 8 = 8
+// 9 = 9
+// 10 = 10
+// Outer loop value 2
+// 0 = 0
+// 2 = 2
+// 4 = 4
+// 6 = 6
+// 8 = 8
+// 10 = 10
+// 12 = 12
+// 14 = 14
+// 16 = 16
+// 18 = 18
+// 20 = 20
+// Outer loop value 3
+// 0 = 0
+// 3 = 3
+// 6 = 6
+// 9 = 9
+// 12 = 12
+// 15 = 15
+// 18 = 18
+// 21 = 21
+// 24 = 24
+// 27 = 27
+// 30 = 30
+// Outer loop value 4
+// 0 = 0
+// 4 = 4
+// 8 = 8
+// 12 = 12
+// 16 = 16
+// 20 = 20
+// 24 = 24
+// 28 = 28
+// 32 = 32
+// 36 = 36
+// 40 = 40
+// Outer loop value 5
+// 0 = 0
+// 5 = 5
+// 10 = 10
+// 15 = 15
+// 20 = 20
+// 25 = 25
+// 30 = 30
+// 35 = 35
+// 40 = 40
+// 45 = 45
+// 50 = 50
+// Outer loop value 6
+// 0 = 0
+// 6 = 6
+// 12 = 12
+// 18 = 18
+// 24 = 24
+// 30 = 30
+// 36 = 36
+// 42 = 42
+// 48 = 48
+// 54 = 54
+// 60 = 60
+// Outer loop value 7
+// 0 = 0
+// 7 = 7
+// 14 = 14
+// 21 = 21
+// 28 = 28
+// 35 = 35
+// 42 = 42
+// 49 = 49
+// 56 = 56
+// 63 = 63
+// 70 = 70
+// Outer loop value 8
+// 0 = 0
+// 8 = 8
+// 16 = 16
+// 24 = 24
+// 32 = 32
+// 40 = 40
+// 48 = 48
+// 56 = 56
+// 64 = 64
+// 72 = 72
+// 80 = 80
+// Outer loop value 9
+// 0 = 0
+// 9 = 9
+// 18 = 18
+// 27 = 27
+// 36 = 36
+// 45 = 45
+// 54 = 54
+// 63 = 63
+// 72 = 72
+// 81 = 81
+// 90 = 90
+// Outer loop value 10
+// 0 = 0
+// 10 = 10
+// 20 = 20
+// 30 = 30
+// 40 = 40
+// 50 = 50
+// 60 = 60
+// 70 = 70
+// 80 = 80
+// 90 = 90
+// 100 = 100
+
+let myArray = ["Flash", "Batman", "Superman"]
+// console.log(myArray.length)  //3
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+// Flash
+// Batman
+// Superman
+
+
+
+
+//break, continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index === 5){
+//         console.log(('Detected 5'));
+//         break;
+//     }
+//     console.log(`Value of i is ${index}`)
+    
+// }
+// Value of i is 1
+// Value of i is 2
+// Value of i is 3
+// Value of i is 4
+// Detected 5
+
+for (let index = 1; index <= 20; index++) {
+    if(index === 5){
+        console.log(('Detected 5'));
+        continue;
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
+// Value of i is 1
+// Value of i is 2
+// Value of i is 3
+// Value of i is 4
+// Detected 5
+// Value of i is 6
+// Value of i is 7
+// Value of i is 8
+// Value of i is 9
+// Value of i is 10
+// Value of i is 11
+// Value of i is 12
+// Value of i is 13
+// Value of i is 14
+// Value of i is 15
+// Value of i is 16
+// Value of i is 17
+// Value of i is 18
+// Value of i is 19
+// Value of i is 20
