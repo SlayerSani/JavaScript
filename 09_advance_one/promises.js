ae9169d305b37f7626b8ed7dd78c2d2df5d65acf
SlayerSani <sanisourin1999@gmail.com>
2024-03-03 02:13:45 +0530

    promises.js

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,46 +1,143 @@
-// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+// // The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
 
-// A Promise is in one of these states:
+// // A Promise is in one of these states:
 
-// pending: initial state, neither fulfilled nor rejected.
-// fulfilled: meaning that the operation was completed successfully.
-// rejected: meaning that the operation failed.
+// // pending: initial state, neither fulfilled nor rejected.
+// // fulfilled: meaning that the operation was completed successfully.
+// // rejected: meaning that the operation failed.
 
-// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+// // MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+
+// const promiseOne = new Promise(function(resolve, reject){
+//     // do n async task
+//     //DB calls, cryptography, network
+//     setTimeout(function(){
+//         console.log('Async task is complete');
+//         resolve()
+//     },1000)
+// })
+
+// promiseOne.then(function(){
+//     console.log("promise consumed");
+// })
+
+
+// // const promiseTwo = 
+// // new Promise((resolve, reject){
+// //     setTimeout(function(){
+// //         console.log("Async task 2");
+// //         resolve();
+// //     })
+// // }).then(function(){
+// //     console.log("Async 2 resoled");
+// // })
+
+// new Promise((resolve, reject) => { // Added arrow function and parameters
+//     setTimeout(() => { // Changed to arrow function
+//         console.log("Async task 2");
+//         resolve();
+//     }, 1000); // Specified timeout duration in milliseconds
+// }).then(() => { // Removed unnecessary function parameter
+//     console.log("Async 2 resolved");
+// });
+
+
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve({username: "Chai", email: "chai@example.com"})
+//     }, 1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
+
+// const promiseFour = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true;
+//         if(!error){
+//             resolve({username:'sani', password:'123'})
+//         }else{
+//             reject('ERROR : SOMETHING WENT WRONG')
+//         }
+//     }, 1000)
+// })
+// promiseFour
+// .then((user)=>{
+//     console.log(user);
+//     return user.username
+// })
+// .then((username) => {
+//     console.log(username);
+// })
+// .catch(function(error){
+//     console.log(error);
+// })
+// .finally(() => console.log("The promise is either resolved or rejected"))
+
+
+// const promiseFive = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true;
+//         if(!error){
+//             resolve({username:'JavaScript', password:'123'})
+//         }else{
+//             reject('ERROR : JS WENT WRONG')
+//         }
+//     }, 1000)
+// })
+// // async function consumePromiseFive (){
+// //     const response = await promiseFive
+// //     console.log(response);
+// // }
+// // console.log(consumePromiseFive);
+
+// async function consumePromiseFive(){
+//     try{
+//         const response = await promiseFive
+//         console.log(response);
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// consumePromiseFive();
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         console.log(response);
+//         // const data = await response.json();
+//         // console.log(data);
+//     } catch (error) {
+//         console.error("E: ", error); // Corrected the error logging
+//     }
+// }
+// getAllUsers();
+
 
 const promiseOne = new Promise(function(resolve, reject){
-    // do n async task
-    //DB calls, cryptography, network
+    //Do an async task
+    // DB calls, cryptography, network
     setTimeout(function(){
-        console.log('Async task is complete');
+        console.log('Async task is compelete');
         resolve()
-    },1000)
+    }, 1000)
 })
 
 promiseOne.then(function(){
-    console.log("promise consumed");
+    console.log("Promise consumed");
 })
 
-
-// const promiseTwo = 
-// new Promise((resolve, reject){
-//     setTimeout(function(){
-//         console.log("Async task 2");
-//         resolve();
-//     })
-// }).then(function(){
-//     console.log("Async 2 resoled");
-// })
-
-new Promise((resolve, reject) => { // Added arrow function and parameters
-    setTimeout(() => { // Changed to arrow function
+new Promise(function(resolve, reject){
+    setTimeout(function(){
         console.log("Async task 2");
-        resolve();
-    }, 1000); // Specified timeout duration in milliseconds
-}).then(() => { // Removed unnecessary function parameter
-    console.log("Async 2 resolved");
-});
+        resolve()
+    }, 1000)
 
+}).then(function(){
+    console.log("Async 2 resolved");
+})
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
@@ -54,63 +151,70 @@ promiseThree.then(function(user){
 
 const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
-        if(!error){
-            resolve({username:'sani', password:'123'})
-        }else{
-            reject('ERROR : SOMETHING WENT WRONG')
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
         }
     }, 1000)
 })
-promiseFour
-.then((user)=>{
+
+ promiseFour
+ .then((user) => {
     console.log(user);
     return user.username
-})
-.then((username) => {
+}).then((username) => {
     console.log(username);
-})
-.catch(function(error){
+}).catch(function(error){
     console.log(error);
-})
-.finally(() => console.log("The promise is either resolved or rejected"))
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
 
 
 const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true;
-        if(!error){
-            resolve({username:'JavaScript', password:'123'})
-        }else{
-            reject('ERROR : JS WENT WRONG')
+        let error = true
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
         }
     }, 1000)
-})
-// async function consumePromiseFive (){
-//     const response = await promiseFive
-//     console.log(response);
-// }
-// console.log(consumePromiseFive);
+});
 
 async function consumePromiseFive(){
-    try{
+    try {
         const response = await promiseFive
         console.log(response);
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
 
-consumePromiseFive();
+consumePromiseFive()
 
-async function getAllUsers(){
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        console.log(response);
-        // const data = await response.json();
-        // console.log(data);
-    } catch (error) {
-        console.error("E: ", error); // Corrected the error logging
-    }
-}
-getAllUsers();
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
